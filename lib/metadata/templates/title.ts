c2278f63a8ff77cb855825e10ae23c04f66d9888
anton gulak <antongulak7@gmail.com>
2024-10-24 14:16:37 +0400

    eslint-disable-next-line @typescript-eslint/no-unused-vars

--- lib/metadata/templates/title.ts
+++ lib/metadata/templates/title.ts
@@ -1,5 +1,6 @@
 import type { Route } from 'nextjs-routes';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function make(pathname: Route['pathname'], isEnriched: boolean) {
   return 'Cypher Testnet Epoch 1 | Zero1 Labs';
 }
